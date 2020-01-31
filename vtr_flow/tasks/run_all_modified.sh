60f0a1d5afdebe5866f9157cb9ab0ffbc43ef9d1
Seyedramin Rasoulinezhad <ramin_r348@yahoo.com>
2020-01-31 13:03:53 +1100

    update

--- vtr_flow/tasks/run_all_modified.sh
+++ vtr_flow/tasks/run_all_modified.sh
@@ -1,12 +1,3 @@
-vtr_dir=~/workspace/vtr/
-
-#./../../vpr/vpr ../arch/timing/xc6vlx240tff1156_modified.xml ../benchmarks/blif/seq.blif
-#exit 0
-
-../scripts/run_vtr_task.pl ramin_task
-exit 0
-
-
 # find all tasks with a config
 tasks=$(find . -name "regression_*" -prune -o -name "config" -print)
 
@@ -15,7 +6,5 @@ do
 	striped="${task%/config}"
     striped="${striped#./}"
     echo "${striped}"	
-    echo ""
-    ~/workspace/vtr-verilog-to-routing/vtr_flow/scripts/run_vtr_task.pl $striped
-    
+    ~/vtr-verilog-to-routing/vtr_flow/scripts/run_vtr_task.pl $striped    
 done
